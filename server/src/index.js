7f8bbc52ae35baaf8304980bb39bbfd4f1be67c9
Rishabh503 <rishabhtripathi2022@gmail.com>
2026-03-05 00:29:51 +0530

    deploy fixes

--- server/src/index.js
+++ server/src/index.js
@@ -8,12 +8,15 @@ import cors from "cors";
 import path from "path"
 
 import { app,server } from "./lib/socket.js"
+
 dotenv.config()
-// const app=express() 
-const __dirname=path.resolve()
+
+const __dirname = path.resolve()
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json({limit:"2mb"}))
 app.use(cookieParser())
+
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
@@ -23,14 +26,14 @@ app.use('/api/auth',authRouter)
 app.use('/api/messages',messageRouter)
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist ")))
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get("/*",(req,res)=>{
+    app.get(/.*/, (req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
 }
 
-server.listen(5001,()=>{
-    console.log("server is running on the port 5001")
+server.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
     connectDb()
 })
